039d616c388efbb00b0200aadbf33da047e596ba
Anas Programmer <anas.fuitsu@gmail.com>
2020-06-04 14:42:37 +0300

    Update api_php.php

--- api_php.php
+++ api_php.php
@@ -36,6 +36,37 @@ return  $str;
 
 
 
+$cmd=$_POST['cmd'];
+$data =$_POST['data'];
+$obj = json_decode($data);
+
+$DevName =$obj->DevName;
+$DevIp  = $obj->DevIP;
+$DevType  =$obj->DevType;
+$DevVer  =$obj->DevVer;
+
+if($cmd=="GetIntnxos")
+{
+$command = NexApiGet($DevIp,$DevVer,$DevType,"sh int ");
+}
+elseif($cmd=="GetIntModenxos")
+{
+$command = NexApiGet($DevIp,$DevVer,$DevType,"show interface switchport");
+}elseif($cmd=="GetVernxos")
+{
+$command = NexApiGet($DevIp,$DevVer,$DevType,"show version ;sh system resources ;sh environment");
+}elseif($cmd=="GetnxSn")
+{
+$command = NexApiGet($DevIp,$DevVer,$DevType,"sh inventory module");
+}elseif($cmd=="GetIntSfpPowernxos")
+{
+$command = NexApiGet($DevIp,$DevVer,$DevType,"show interface transceiver details");
+}
+elseif($cmd=="GetVlansAllnxos")
+{
+$command = NexApiGet($DevIp,$DevVer,$DevType,"sh vlan summary ;show vlan");
+}
+
 
 
 
